927c8a046dc399a8ab454013c1fc85bedb9b79a2
parkseongsoo <102282941+parkseongsooo@users.noreply.github.com>
2023-02-01 21:39:38 +0900

    c06

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongsop <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 15:02:59 by seongsop          #+#    #+#             */
+/*   Updated: 2023/01/24 12:23:07 by seongsop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i--;
+	}
+	return (0);
+}
